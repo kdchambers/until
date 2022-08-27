9d14973c5cac3e8079bfdcb8d0339446128c342f
Keith Chambers <kd.chambers97@gmail.com>
2022-08-27 17:09:52 -0500

    Added license comment header to src/main.zig

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0
+// Copyright (c) 2022 Keith Chambers
+// This program is free software: you can redistribute it and/or modify it under the terms
+// of the GNU General Public License as published by the Free Software Foundation, version 3.
+
 const std = @import("std");
 const c = @cImport({
     @cInclude("time.h");
